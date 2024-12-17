c914c236b7fee5246b0fd7da477d7a5357efbd41
Kyle Carberry <kyle@carberry.com>
2024-12-17 08:51:17 -0500

    Refactor Node constructor to require mode argument
    - Replace the optional permission parameter with a required mode parameter
    - Update test cases and internal logic to use the `mode` instead of separate `perm` settings
    - Simplify node type management by directly assigning mode values

--- src/__tests__/node.test.ts
+++ src/__tests__/node.test.ts
@@ -3,9 +3,9 @@ import { constants } from '../constants';
 
 describe('node.ts', () => {
   describe('Node', () => {
-    const node = new Node(1);
+    const node = new Node(1, constants.S_IFREG | 0o666);
     it('properly sets mode with permission respected', () => {
-      const node = new Node(1, 0o755);
+      const node = new Node(1, constants.S_IFREG | 0o755);
       expect(node.perm).toBe(0o755);
       expect(node.mode).toBe(constants.S_IFREG | 0o755);
       expect(node.isFile()).toBe(true); // Make sure we still know it's a file
@@ -18,33 +18,33 @@ describe('node.ts', () => {
     });
     describe('.write(buf, off, len, pos)', () => {
       it('Simple write into empty node', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         node.write(Buffer.from([1, 2, 3]));
         expect(node.getBuffer().equals(Buffer.from([1, 2, 3]))).toBe(true);
       });
       it('Append to the end', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         node.write(Buffer.from([1, 2]));
         node.write(Buffer.from([3, 4]), 0, 2, 2);
         const result = Buffer.from([1, 2, 3, 4]);
         expect(node.getBuffer().equals(result)).toBe(true);
       });
       it('Overwrite part of the buffer', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         node.write(Buffer.from([1, 2, 3]));
         node.write(Buffer.from([4, 5, 6]), 1, 2, 1);
         const result = Buffer.from([1, 5, 6]);
         expect(node.getBuffer().equals(result)).toBe(true);
       });
       it('Overwrite part of the buffer and extend', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         node.write(Buffer.from([1, 2, 3]));
         node.write(Buffer.from([4, 5, 6, 7]), 0, 4, 2);
         const result = Buffer.from([1, 2, 4, 5, 6, 7]);
         expect(node.getBuffer().equals(result)).toBe(true);
       });
       it('Write outside the space of the buffer', () => {
-        const node = new Node(1);
+        const node = new Node(1,0);
         node.write(Buffer.from([1, 2, 3]));
         node.write(Buffer.from([7, 8, 9]), 0, 3, 6);
         node.write(Buffer.from([4, 5, 6]), 0, 3, 3);
@@ -54,14 +54,14 @@ describe('node.ts', () => {
     });
     describe('.read(buf, off, len, pos)', () => {
       it('Simple one byte read', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         node.write(Buffer.from([1, 2, 3]));
         const buf = Buffer.allocUnsafe(1);
         node.read(buf, 0, 1, 1);
         expect(buf.equals(Buffer.from([2]))).toBe(true);
       });
       it('updates the atime and ctime', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         const oldAtime = node.atime;
         const oldCtime = node.ctime;
         node.read(Buffer.alloc(0));
@@ -72,16 +72,16 @@ describe('node.ts', () => {
       });
     });
     describe('.chmod(perm)', () => {
-      const node = new Node(1);
+      const node = new Node(1, constants.S_IFREG | 0o666);
       expect(node.perm).toBe(0o666);
       expect(node.isFile()).toBe(true);
       node.chmod(0o600);
       expect(node.perm).toBe(0o600);
       expect(node.isFile()).toBe(true);
     });
-    describe.each(['uid', 'gid', 'atime', 'mtime', 'perm', 'nlink'])('when %s changes', field => {
+    describe.each(['uid', 'gid', 'atime', 'mtime', 'nlink'])('when %s changes', field => {
       it('updates the property and the ctime', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         const oldCtime = node.ctime;
         node[field] = 1;
         const newCtime = node.ctime;
@@ -91,7 +91,7 @@ describe('node.ts', () => {
     });
     describe('.getString(encoding?)', () => {
       it('updates the atime and ctime', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         const oldAtime = node.atime;
         const oldCtime = node.ctime;
         node.getString();
@@ -103,7 +103,7 @@ describe('node.ts', () => {
     });
     describe('.getBuffer()', () => {
       it('updates the atime and ctime', () => {
-        const node = new Node(1);
+        const node = new Node(1, 0);
         const oldAtime = node.atime;
         const oldCtime = node.ctime;
         node.getBuffer();
